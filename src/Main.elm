093d57d47382a6b22659acf66ccf11f6fba57cff
Fernando Mauricio Mamani Navarro <wmauchess@gmail.com>
2024-07-21 11:52:25 -0400

    feat: main page

---
+++ src/Main.elm
@@ -0,0 +1,35 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, div, h1, text)
+
+-- Modelo
+type alias Model =
+    { message : String }
+
+-- Inicialización del modelo
+init : Model
+init =
+    { message = "Fernando Mauricio Mamani Navarro" }
+
+-- Mensajes
+type Msg
+    = NoOp
+
+-- Actualización del modelo
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NoOp ->
+            model
+
+-- Vista
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text model.message ]
+        ]
+
+-- Programa principal
+main =
+    Browser.sandbox { init = init, update = update, view = view }
